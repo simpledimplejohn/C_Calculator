b60e9ded267dbb8d002622f4e7414cb4b76de5ec
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-10 14:32:57 -0700

    add UI to runCalculator

--- methods/runCalculator.cs
+++ methods/runCalculator.cs
@@ -9,8 +9,17 @@ namespace Calculator.runStuff
     string works = "runCalculator works!";
     public void runCalculator()
     {
-      Console.WriteLine(works);
+      Console.WriteLine("welcome to the calculator");        
+      Console.WriteLine("enter your first number");
+      int num1 = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("enter your second number");
+      int num2 = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("what would you like to do? + - / * ");
+      string val = (Console.ReadLine());
       newCalculations.runAdd();
+      Console.WriteLine(num1);
+      Console.WriteLine(num2);
+      Console.WriteLine(val);
     }
   }
 }
